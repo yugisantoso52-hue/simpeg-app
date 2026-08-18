e83a332783b219623a2c40cc99bb2ae372445ef2
yugisantoso52-hue <yugisantoso52@gmail.com>
2026-08-18 15:37:38 +0700

    Fix HTTPS dan Error 419

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Memaksa penggunaan HTTPS di server Production (Mengatasi Error 419 & Tampilan Berantakan)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         /*
         |--------------------------------------------------------------------------
         | Enterprise Pagination
