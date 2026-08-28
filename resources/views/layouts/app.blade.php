33a391219e77076894a6ebbf0d25fb56b01ab755
yugisantoso52-hue <yugisantoso52@gmail.com>
2026-08-28 14:08:19 +0700

    fix(layout): add @yield(content) fallback in app.blade.php to support views using @extends

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,17 +19,26 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
+            @if(isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @elseif(View::hasSection('header'))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        @yield('header')
+                    </div>
+                </header>
+            @endif
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @if(isset($slot))
+                    {{ $slot }}
+                @endif
+                @yield('content')
             </main>
         </div>
     </body>
