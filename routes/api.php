159935ed56b3ea460468862f623f2b668622378a
yugisantoso52-hue <yugisantoso52@gmail.com>
2026-09-25 08:57:32 +0700

    Update api.php with deploy-webhook route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SyncController;
+use App\Http\Controllers\DeployWebhookController;
 
 Route::post('/v1/sync/receive', [SyncController::class, 'receive']);
 Route::get('/v1/sync/changes', [SyncController::class, 'changes']);
+
+/* Automated 1-Click Deployment Webhook */
+Route::match(['get', 'post'], '/deploy-webhook', [DeployWebhookController::class, 'handle']);
